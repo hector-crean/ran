219488ff8f9265c6fff15e72bcdac345de9e1922
Hector Crean <hectorcrean@gmail.com>
2025-07-03 15:20:57 +0100

    add drawers/seet

--- src/types/slides.ts
+++ src/types/slides.ts
@@ -1,5 +1,7 @@
 import { VideoSlideProps } from "@/app/video-slideshow/slide-types/video-slide";
 import {  RotationalSequenceSlideProps} from "@/app/video-slideshow/slide-types/rotational-sequence-slide";
+import { FreezeFrameProps } from "@/app/video-slideshow/slide-types/freeze-frame";
+import { ReactNode } from "react";
 
 type SlideLike<T extends string,P> = { type: T, data: P};
 
@@ -26,18 +28,37 @@ type PollSlide = SlideLike<"Poll", {
 
 type RotationalSequenceSlide = SlideLike<"RotationalSequence", RotationalSequenceSlideProps>;
 
+
+type FreezeFrameSlide = SlideLike<"FreezeFrame", FreezeFrameProps>;
+
     
 export type SlideType =
     | RegularSlide
     | VideoSlide
     | InteractiveSlide
     | PollSlide
-    | RotationalSequenceSlide;
+    | RotationalSequenceSlide
+    | FreezeFrameSlide;
 
 export interface Slide {
   id: string;
   title: string;
   firstFramePoster: string;
   lastFramePoster: string;
+  initialDrawer: ReactNode | null,
+  initialSheet: ReactNode | null,
+  initialDialog: ReactNode | null,
   slide_type: SlideType;
 } 
+
+
+
+
+
+type RenderableNodeLike<T extends string,P> = { type: T, data: P};
+
+type InfiniteCanvasMap = RenderableNodeLike<"InfiniteCanvasMap", {}>
+
+export type RenderableNode = InfiniteCanvasMap;
+
+
